cb5d7acb25f8ed4afa1be3f1a645d917080d4b34
Tobija Žuntar <tobija.zuntar@gmail.com>
2024-03-04 12:22:03 +0100

    Fix an inconsistency in typeorm config file password field

--- src/config/orm.config.ts
+++ src/config/orm.config.ts
@@ -6,7 +6,7 @@ export const OrmConfig = async (configService: ConfigService): Promise<Connectio
     host: configService.get('DATABASE_HOST') || 'localhost',
     port: parseInt(configService.get('DATABASE_PORT')) || 5432,
     username: configService.get('DATABASE_USER'),
-    password: configService.get('DATABASE_PASSWORD'),
+    password: configService.get('DATABASE_PASS'),
     database: configService.get('DATABASE_NAME'),
     entities: [__dirname + '../../entities/**/*.entity{.ts,.js}'],
     migrations: [__dirname + '../../migrations/**/*.entity{.ts,.js}'],
